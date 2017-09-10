ff51311d2197c234682692898123642ba99d8d42
meriadec <meriadec.pillet@gmail.com>
2017-09-10 23:49:44 +0200

    update conky

--- config/conky/lua/circle.lua
+++ config/conky/lua/circle.lua
@@ -19,7 +19,7 @@ function drawCircle(cr, x, y, value, label)
   --rings size
   ring_center_x=x
   ring_center_y=y
-  ring_radius=100
+  ring_radius=50
   ring_width=3
   --colors
   --set background colors, 1,0,0,1 = fully opaque red
@@ -31,7 +31,7 @@ function drawCircle(cr, x, y, value, label)
   ring_in_red=1
   ring_in_green=1
   ring_in_blue=1
-  ring_in_alpha=0.5
+  ring_in_alpha=0.3
   --indicator value settings
   max_value=100
 
@@ -40,11 +40,21 @@ function drawCircle(cr, x, y, value, label)
   cairo_arc (cr,ring_center_x,ring_center_y,ring_radius,0,2*math.pi)
   cairo_stroke (cr)
 
-  cairo_set_line_width (cr,30)
+  cairo_set_line_width (cr,ring_width)
+  cairo_set_source_rgba (cr,ring_bg_red,ring_bg_green,ring_bg_blue,0.06)
+  cairo_arc (cr,ring_center_x,ring_center_y,ring_radius + 5,0,2*math.pi)
+  cairo_stroke (cr)
+
+  cairo_set_line_width (cr,20)
   end_angle=value*(360/max_value)
-  --print (end_angle)
+  cairo_set_source_rgba (cr,ring_in_red,ring_in_green,ring_in_blue,0.3)
+  cairo_arc (cr,ring_center_x,ring_center_y,ring_radius - 15,(-90)*(math.pi/180),(end_angle-90)*(math.pi/180))
+  cairo_stroke (cr)
+
+  cairo_set_line_width (cr,3)
+  end_angle=value*(360/max_value)
   cairo_set_source_rgba (cr,ring_in_red,ring_in_green,ring_in_blue,ring_in_alpha)
-  cairo_arc (cr,ring_center_x,ring_center_y,ring_radius - 20,(-90)*(math.pi/180),(end_angle-90)*(math.pi/180))
+  cairo_arc (cr,ring_center_x,ring_center_y,ring_radius - 7,(-90)*(math.pi/180),(end_angle-90)*(math.pi/180))
   cairo_stroke (cr)
 
   padded_value = tostring(value)
@@ -52,50 +62,37 @@ function drawCircle(cr, x, y, value, label)
     padded_value = "0" .. padded_value
   end
 
-  drawText(cr, x - 25, y + 5, padded_value, 40, 0.7)
-  drawText(cr, x - 12 * (string.len(label) + 3) / 2, y + 30, "% " .. label, 24, 0.4)
+  drawText(cr, x - 4 * (string.len(label) + 3) / 2, y + 10, label, 18, 0.4)
 end
 
 function conky_main()
+
   if conky_window == nil then
     return
   end
-  local cs = cairo_xlib_surface_create(conky_window.display,
-  conky_window.drawable,
-  conky_window.visual,
-  conky_window.width,
-  conky_window.height)
+
+  local cs = cairo_xlib_surface_create(
+    conky_window.display,
+    conky_window.drawable,
+    conky_window.visual,
+    conky_window.width,
+    conky_window.height
+  )
+
   cr = cairo_create(cs)
 
   cpu=conky_parse("${cpu}")
+  drawCircle(cr, 150, 60, cpu, "CPU")
+
   mem=conky_parse("${memperc}")
-  drawCircle(cr, 140, 150, cpu, "CPU")
-  drawCircle(cr, 140, 390, mem, "MEM")
+  drawCircle(cr, 270, 60, mem, "MEM")
 
-  cairo_set_line_width(cr, 4)
-  cairo_set_line_cap(cr, CAIRO_LINE_CAP_BUTT)
-  cairo_set_source_rgba(cr,1,1,1,0.1)
-  cairo_move_to(cr, 140, 252)
-  cairo_line_to(cr, 140, 288)
-  cairo_stroke(cr)
-
-  -- line_width=1
-  -- line_cap=CAIRO_LINE_CAP_BUTT
-  -- red,green,blue,alpha=1,1,1,0.4
-  -- startx=100
-  -- starty=100
-  -- endx=200
-  -- endy=100
-  -- ----------------------------
   -- cairo_set_line_width(cr, 4)
   -- cairo_set_line_cap(cr, CAIRO_LINE_CAP_BUTT)
-  -- cairo_set_source_rgba(cr,red,green,blue,alpha)
+  -- cairo_set_source_rgba(cr,1,1,1,1)
   -- cairo_move_to(cr, 0, 0)
-  -- cairo_line_to(cr,endx,endy + updates * 4)
-  -- cairo_move_to(cr, 20, 0)
-  -- cairo_line_to(cr,endx,endy + updates * 4)
+  -- cairo_line_to(cr, 0, 100)
   -- cairo_stroke(cr)
-
 
   -- ==============================
   cairo_destroy(cr)
