55eb1b0db7ebf561921a3e7e160ff2191dc217ef
meriadec <meriadec.pillet@gmail.com>
2018-02-13 14:53:25 +0100

    updates

---
+++ config/glava/bars.glsl
@@ -0,0 +1,21 @@
+
+/* center line thickness (pixels) */
+#define C_LINE 5
+
+/* width (in pixels) of each bar */
+#define BAR_WIDTH 8
+/* width (in pixels) of each bar gap */
+#define BAR_GAP 4
+/* outline color */
+#define BAR_OUTLINE #ffffff33
+/* outline width (in pixels, set to 0 to disable outline drawing) */
+#define BAR_OUTLINE_WIDTH 0
+/* Amplify magnitude of the results each bar displays */
+#define AMPLIFY 300
+/* Bar color */
+#define COLOR (#00000022 * ((d / 60) + 1))
+/* Direction that the bars are facing, 0 for inward, 1 for outward */
+#define DIRECTION 0
+/* Whether to switch left/right audio buffers */
+#define INVERT 0
+
