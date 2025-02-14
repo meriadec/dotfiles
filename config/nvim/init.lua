6044a728cee736b52db884c85f45eeb558365845
Meriadec Pillet <meriadec.pillet@gmail.com>
2025-02-14 16:35:18 +0100

    upd

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -508,6 +508,19 @@ require("lazy").setup({
     end
   },
 
+  -- copilot
+  {
+    "github/copilot.vim",
+    config = function()
+      -- remap accept completion to <C-J> because <tab> is used for normal completion (e.g LSP)
+      vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false
+      })
+      vim.g.copilot_no_tab_map = true
+    end
+  },
+
   -- prisma coloration
   { "prisma/vim-prisma" }
 }, opts)
