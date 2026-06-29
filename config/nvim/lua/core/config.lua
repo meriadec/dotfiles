80c58f57fda98f0122ecf437bf173e96564d1ddb
Meriadec Pillet <meriadec.pillet@gmail.com>
2026-06-29 08:55:50 +0200

    upd

--- config/nvim/lua/core/config.lua
+++ config/nvim/lua/core/config.lua
@@ -30,6 +30,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.hl_op()
   end,
 })
